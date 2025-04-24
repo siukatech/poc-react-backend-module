bee4fb441bb27f0bfa54dd25bd4b24f65d7f5fbe
siukatech <siuka114@gmail.com>
2025-04-25 02:00:41 +0800

    fix: remove Named annotation from AbstractMapper to enable auto mapping

--- react-backend-module-core/src/main/java/com/siukatech/poc/react/backend/module/core/business/mapper/AbstractMapper.java
+++ react-backend-module-core/src/main/java/com/siukatech/poc/react/backend/module/core/business/mapper/AbstractMapper.java
@@ -5,13 +5,15 @@ import org.mapstruct.Named;
 import java.util.Optional;
 
 public interface AbstractMapper {
-    @Named("wrapOptional")
+
+//    @Named("wrapOptional")
     default <T> Optional<T> wrapOptional(T obj) {
         return Optional.ofNullable(obj);
     }
 
-    @Named("unwrapOptional")
+//    @Named("unwrapOptional")
     default <T> T unwrapOptional(Optional<T> objOptional) {
         return objOptional == null ? null : objOptional.orElse(null);
     }
+
 }
